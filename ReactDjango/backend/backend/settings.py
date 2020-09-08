350854c711e7537ab79cf00f742b9c5455a03e14
adam-paz <adampaz8@gmail.com>
2020-09-08 14:58:31 -0700

    connected to remote DB

--- ReactDjango/backend/backend/settings.py
+++ ReactDjango/backend/backend/settings.py
@@ -74,19 +74,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
-     'default': {
-         'ENGINE': 'djongo',
-         'NAME': 'your-db-name',
-     }
- }
-
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'Dev-Test',
+        'CLIENT': {
+           'host': ,
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
