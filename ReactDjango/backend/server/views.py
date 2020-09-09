2cf5d653945c9fe3b0cf631625867339e5d52c3a
adam-paz <adampaz8@gmail.com>
2020-09-09 16:15:58 -0700

    security file added

--- ReactDjango/backend/server/views.py
+++ ReactDjango/backend/server/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from server.models import Board
+from .models import Board
 
 def home(request):
+    #boards = Board.objects.all()
+    #return render(request, '../../client/public/index.html')
+    #, {'boards': boards})
+    
     boards = Board.objects.all()
-    boards_names = list()
-
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
