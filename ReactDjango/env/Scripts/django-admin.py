296d301ad81e51d1d3a31df2332978db8c4f16e6
Daniel Hannani <hannani.daniel@gmail.com>
2020-09-11 15:56:14 -0700

    removed env

--- ReactDjango/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\adam\desktop\project\nidampy\reactdjango\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
